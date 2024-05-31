c7df1db72d6b0354e4898fc40a0df0aa82ecb64d
shwetaTh <shwetaTh@users.noreply.github.com>
2024-05-31 22:59:33 +0545

    created a data model for hospital management system

---
+++ BasicProject/Backend/models/hospital management/patient.models.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const patientSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        diagnosedWith:{
+            type: String,
+            required: true
+        },
+        address:{
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        bloodGroud:{
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String,
+            enum: ["M", "F", "Other"],
+            required:true
+        },
+        admittedIn:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital"
+        }
+    },{timestamps: true})
+export const Patient = mongoose.model("Patient", patientSchema)    
